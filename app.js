6dd0d68c5693c1523df98e072b36b39c97d5030e
Yusuf Çelik <58196464+GioGio7777@users.noreply.github.com>
2021-03-30 15:59:35 +0300

    inputField da bir sorun vardı çözüldü

--- app.js
+++ app.js
@@ -1,38 +1,25 @@
-const inputField = document.getElementById("inputField");
-const time = document.getElementById("trend");
-const memeME = document.getElementById("reddit");
-const range = document.getElementById("range");
-const t = document.getElementById("time");
+const inputField = document.getElementById('inputField');
+const time = document.getElementById('trend');
+const memeME = document.getElementById('reddit');
+const range = document.getElementById('range');
+const t = document.getElementById('time');
 //import UI from "./ui";
 const ui = new UI();
 
-
-
 eventListeners();
 
-
 function eventListeners() {
-
-    document.addEventListener('DOMContentLoaded', myFunction);
-    inputField.addEventListener("change", lookingInputField);
-    memeME.addEventListener("click", memeTime);
-    range.addEventListener("click", rangeValue);
-    trend.addEventListener("click", showAnother);
-
-
-
-
+  document.addEventListener('DOMContentLoaded', myFunction);
+  inputField.addEventListener('change', lookingInputField);
+  memeME.addEventListener('click', memeTime);
+  range.addEventListener('click', rangeValue);
+  trend.addEventListener('click', showAnother);
 }
 
-function myFunction(e) {
-
-
-
-}
+function myFunction(e) {}
 
 function showAnother() {
-
-    /*
+  /*
     <select class="form-select" id="time" aria-label="Disabled select example">
         <option selected>Now</option>
         <option value="New">Today</option>
@@ -44,105 +31,76 @@ function showAnother() {
 
 */
 
-    console.log(time.value);
+  console.log(time.value);
 
-    if (time.value === "Top") {
-        console.log("se");
-        var form = document.createElement("select");
-        form.className = "form-select";
-        form.id = "time";
+  if (time.value === 'Top') {
+    console.log('se');
+    var form = document.createElement('select');
+    form.className = 'form-select';
+    form.id = 'time';
 
+    var option = document.createElement('option');
+    option.value = 'Now';
+    option.text = 'Now';
+    form.appendChild(option);
 
+    var option = document.createElement('option');
+    option.value = 'This Week';
+    option.text = 'This Week';
+    form.appendChild(option);
 
-        var option = document.createElement("option");
-        option.value = "Now";
-        option.text = "Now";
-        form.appendChild(option);
+    var option = document.createElement('option');
+    option.value = 'This Month';
+    option.text = 'This Month';
+    form.appendChild(option);
 
+    var option = document.createElement('option');
+    option.value = 'This Year';
+    option.text = 'This Year';
+    form.appendChild(option);
 
-        var option = document.createElement("option");
-        option.value = "This Week";
-        option.text = "This Week";
-        form.appendChild(option);
+    var option = document.createElement('option');
+    option.value = 'All Time';
+    option.text = 'All Time';
+    form.appendChild(option);
 
-        var option = document.createElement("option");
-        option.value = "This Month";
-        option.text = "This Month";
-        form.appendChild(option);
-
-
-        var option = document.createElement("option");
-        option.value = "This Year";
-        option.text = "This Year";
-        form.appendChild(option);
-
-
-        var option = document.createElement("option");
-        option.value = "All Time";
-        option.text = "All Time";
-        form.appendChild(option);
-
-        /*Bir döngü ile  de basitce ve daha az satırla halledilebilir 
+    /*Bir döngü ile  de basitce ve daha az satırla halledilebilir 
         ama ben böyle uygun gördüm.
         */
 
-        document.getElementById("input").appendChild(form);
-
-
-
-
-
-
-    }
-
-
-
-
+    document.getElementById('input').appendChild(form);
+  }
 }
 
-
 function lookingInputField(e) {
-
-    time.disabled = false;
-
-
-
+  if (inputField.value === '') {
+    time.disabled = true;
+  } else time.disabled = false;
 }
 
 function memeTime() {
+  var subre = inputField.value;
+  var tt = time.value;
+  var rng = Number(range.value);
 
+  document.getElementById('input').remove();
+  fetch(`https://www.reddit.com/r/${subre}/${tt}/.json?limit=${rng}`)
+    .then((response) => response.json())
+    .then((data) => {
+      for (var j = 0; j < rng; j++) {
+        const datas = data.data;
+        const children = datas.children;
+        const meme = children[j].data;
+        const subreddit = meme.subreddit;
+        const title = meme.title;
+        const upvote = meme.ups;
+        const image = meme.url;
+        const id = meme.id;
+        const author = meme.author;
 
+        console.log(image);
 
-    var subre = inputField.value;
-    var tt = time.value;
-    var rng = Number(range.value);
-
-    document.getElementById("input").remove();
-    fetch(`https://www.reddit.com/r/${subre}/${tt}/.json?limit=${rng}`)
-        .then(response => response.json())
-        .then(data => {
-
-            for (var j = 0; j < rng; j++) {
-
-                const datas = data.data;
-                const children = datas.children;
-                const meme = children[j].data;
-                const subreddit = meme.subreddit;
-                const title = meme.title;
-                const upvote = meme.ups;
-                const image = meme.url;
-                const id = meme.id;
-                const author = meme.author;
-
-
-
-
-
-
-                console.log(image);
-                
-
-            /*   try {
+        /*   try {
                     document.getElementById("meme").innerHTML += ` 
         <div class="card" style="width: 40rem;">
         <img src="${image}" class="card-img-top" alt="error">
@@ -169,33 +127,21 @@ function memeTime() {
 
                 */
 
+        var mem = {
+          subreddit: subreddit,
+          title: title,
+          upvote: upvote,
+          image: image,
+          id: id,
+          author: author,
+        };
 
-
-                var mem = {
-                    subreddit: subreddit,
-                    title: title,
-                    upvote: upvote,
-                    image: image,
-                    id: id,
-                    author: author
-
-                };
-
-                ui.getValue(mem, subre);
-             ui.userInterface();
-
-
-
-
-
-            }
-        })
-
+        ui.getValue(mem, subre);
+        ui.userInterface();
+      }
+    });
 }
 
-
 function rangeValue() {
-
-    const span = document.getElementById("rangeValue").innerHTML = range.value;
-
+  const span = (document.getElementById('rangeValue').innerHTML = range.value);
 }
